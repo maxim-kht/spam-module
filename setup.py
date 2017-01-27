b5175d3f1b5b8c35286f640f61679e3393ddaec6
Maxim Kukhtenkov <maxim.kht@gmail.com>
2017-01-26 17:04:07 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup, Extension
+
+
+spam_module = Extension('spammodule', sources=['spammodule.c'])
+
+setup(name='Spam',
+      version='1.0',
+      description='Practice module for writing C extensions',
+      ext_modules=[spam_module])
